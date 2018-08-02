a71716e912f10123d9b63047e17d6bbc8aadb352
xianhuawei <258022429@qq.com>
2018-08-02 17:36:48 +0800

    Create install_exchage.sh

---
+++ install_exchage.sh
@@ -0,0 +1,15 @@
+yum install -y centos-release epel-release
+
+#yum install redis 
+
+#mysql
+#rpm -ivh https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-community-common-8.0.12-1.el7.x86_64.rpm
+#rpm -ivh https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-community-libs-8.0.12-1.el7.x86_64.rpm
+#rpm -ivh https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-community-server-8.0.12-1.el7.x86_64.rpm
+
+#rpm -ivh https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-community-client-8.0.12-1.el7.x86_64.rpm
+
+#rpm -ivh exchange_server-1.0-1.x86_64.rpm
+
+#systemctl enable [accesshttp,accessws,alertcenter,marketprice,matchengine,readhistory]
+#systemctl start [accesshttp,accessws,alertcenter,marketprice,matchengine,readhistory]
